1b3cf913cb38d5e63b6dfd23b57d5d9b4f28310a
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-07-21 14:30:41 +0200

    Set the correct module name in the installer.

--- lua-periphery/installer/lua5.4/install_linux.lua
+++ lua-periphery/installer/lua5.4/install_linux.lua
@@ -3,8 +3,7 @@ local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
 if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
-  t:install('lua_plugins/lxp.so',   '${install_lua_cpath}/')
-  t:install('lua',                  '${install_lua_path}/')
+  t:install('lua_plugins/periphery.so', '${install_lua_cpath}/')
   tResult = true
 end
 
